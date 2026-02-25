00406df5ab97b376c2e85076b3532c68f75ce6fd
Storchak Oleksandr <alexsstorchak@gmail.com>
2026-02-25 15:32:47 +0200

    Init

---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_VERSION = "v3";
+const CACHE_NAME = `school-site-${CACHE_VERSION}`;
+const ASSETS = [
+	"./",
+	"index.html",
+	"style.css",
+	"site.webmanifest",
+	"favicon.ico",
+	"favicon-16x16.png",
+	"favicon-32x32.png",
+	"apple-touch-icon.png",
+	"android-chrome-192x192.png",
+    "android-chrome-512x512.png",
+    "icon-maskable-192.png",
+    "icon-maskable-512.png",
+    "screenshot-wide.png",
+    "screenshot-mobile.png"
+];
+
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+	);
+});
+
+self.addEventListener("activate", (event) => {
+	event.waitUntil(
+		caches.keys().then((keys) =>
+			Promise.all(
+				keys
+					.filter((key) => key.startsWith("school-site-") && key !== CACHE_NAME)
+					.map((key) => caches.delete(key))
+			)
+		)
+	);
+	self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+	const { request } = event;
+
+	if (request.method !== "GET") {
+		return;
+	}
+
+	event.respondWith(
+		caches.match(request).then((cached) => {
+			if (cached) {
+				return cached;
+			}
+
+			return fetch(request).then((response) => {
+				if (!response || response.status !== 200 || response.type !== "basic") {
+					return response;
+				}
+
+				const responseClone = response.clone();
+				caches.open(CACHE_NAME).then((cache) => cache.put(request, responseClone));
+				return response;
+			});
+		})
+	);
+});
