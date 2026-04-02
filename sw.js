0859b50bf8d39ee3ccb4360a33325fb65ac5124d
Storchak Oleksandr <alexsstorchak@gmail.com>
2026-04-03 00:26:58 +0300

    Updates

--- sw.js
+++ sw.js
@@ -2,16 +2,16 @@ const CACHE_VERSION = "v7";
 const CACHE_NAME = `school-site-${CACHE_VERSION}`;
 const ASSETS = [
   "site.webmanifest",
-  "favicon.ico",
-  "favicon-16x16.png",
-  "favicon-32x32.png",
-  "apple-touch-icon.png",
-  "android-chrome-192x192.png",
-  "android-chrome-512x512.png",
-  "icon-maskable-192.png",
-  "icon-maskable-512.png",
-  "screenshot-wide.png",
-  "screenshot-mobile.png"
+  "images/favicon.ico",
+  "images/favicon-16x16.png",
+  "images/favicon-32x32.png",
+  "images/apple-touch-icon.png",
+  "images/android-chrome-192x192.png",
+  "images/android-chrome-512x512.png",
+  "images/icon-maskable-192.png",
+  "images/icon-maskable-512.png",
+  "images/screenshot-wide.png",
+  "images/screenshot-mobile.png"
 ];
 
 self.addEventListener("install", (event) => {
